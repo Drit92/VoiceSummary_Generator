c774239abef675473b8a8c9793a9e591d3393154
Drit92 <146371382+Drit92@users.noreply.github.com>
2025-10-14 16:00:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Upgrade pip to the latest version
+# Upgrade pip
 pip install --upgrade pip
 
-# Install dependencies from requirements.txt without cache or building from source
-pip install --no-cache-dir --no-binary :all: -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt
 
-# Create the Streamlit config directory (if it doesn't exist)
+# Create Streamlit config directory
 mkdir -p ~/.streamlit/
 
-# Streamlit server configuration
+# Write Streamlit configuration
 echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
+port = $PORT\n\
 \n\
 " > ~/.streamlit/config.toml
