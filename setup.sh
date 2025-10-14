727825f094a96bdf272bb437727a681873eff166
Drit92 <146371382+Drit92@users.noreply.github.com>
2025-10-14 11:35:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
+# Upgrade pip to the latest version
 pip install --upgrade pip
 
-
+# Install dependencies from requirements.txt without cache or building from source
 pip install --no-cache-dir --no-binary :all: -r requirements.txt
 
-
+# Create the Streamlit config directory (if it doesn't exist)
 mkdir -p ~/.streamlit/
 
 # Streamlit server configuration
